d8cf734b0ed142379cf71661678a73b2b7afb6fa
karinelisboa <karinelisboa.20232@poli.ufrj.br>
2025-04-19 02:37:03 -0300

    Configurei o admin para Ativo e Cotação

--- monitoramento/admin.py
+++ monitoramento/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Ativo, Cotacao
 
-# Register your models here.
+@admin.register(Ativo)
+class AtivoAdmin(admin.ModelAdmin):
+    list_display = ('codigo', 'nome', 'tipo_tunel', 'limite_inferior', 'limite_superior', 'percentual_tunel', 'periodicidade_minutos')
+    search_fields = ('codigo', 'nome')
+
+@admin.register(Cotacao)
+class CotacaoAdmin(admin.ModelAdmin):
+    list_display = ('ativo', 'preco', 'data_hora')
+    list_filter = ('ativo',)
+    search_fields = ('ativo__codigo',)
